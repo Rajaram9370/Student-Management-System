b4359cc5743da3a0d3ba4f62532f5ad45e6eed18
Rajaram9370 <rajaram9370@gmail.com>
2026-06-29 15:23:41 +0530

    Fix CSRF trusted origins and allowed hosts for local development

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -14,7 +14,11 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
+SESSION_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SAMESITE = 'Lax'
 
 
 # Application definition
